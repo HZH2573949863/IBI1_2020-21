bf3022f4f91c3c2038eca566b1be98ad6673ce23
HZH2573949863 <79899900+HZH2573949863@users.noreply.github.com>
2021-03-10 14:05:30 +0800

    edit variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,9 +1,14 @@
 a=20208
 b=190784
 c=210310
-d=c-a
-e=b-a
-print(d-e)
+d= abs(a-c)
+e= abs(a-b)
+if d < e:
+ print ("d<e")
+elif d > e:
+ print ("d>e")
+else:
+ print ("d=e")
 x=(4>0)
 y=(2>5)
 z=(x and not y)or(y and not x)
